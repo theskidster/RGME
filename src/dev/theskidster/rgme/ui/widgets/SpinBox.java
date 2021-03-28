cee4d379667a3dfafe54f5140b1ebdf3b2b82355
theskidster <theskidster@gmail.com>
2021-03-27 23:57:41 -0500

    added buttons to spin box- still requires functionality

--- src/dev/theskidster/rgme/ui/widgets/SpinBox.java
+++ src/dev/theskidster/rgme/ui/widgets/SpinBox.java
@@ -9,6 +9,7 @@ import dev.theskidster.rgme.ui.FreeTypeFont;
 import dev.theskidster.rgme.ui.UI;
 import dev.theskidster.rgme.utils.Color;
 import dev.theskidster.rgme.utils.Mouse;
+import dev.theskidster.rgme.utils.Rectangle;
 import dev.theskidster.rgme.utils.TextInput;
 import static org.lwjgl.glfw.GLFW.*;
 import static org.lwjgl.opengl.GL11.*;
@@ -20,8 +21,17 @@ import static org.lwjgl.opengl.GL11.*;
 
 public class SpinBox extends TextInput {
     
-    private final Icon leftBorder  = new Icon(15, 30);
-    private final Icon rightBorder = new Icon(15, 30);
+    private final Rectangle topButton    = new Rectangle(0, 0, 23, 15);
+    private final Rectangle bottomButton = new Rectangle(0, 0, 23, 15);
+    
+    private final Icon leftBorder    = new Icon(15, 30);
+    private final Icon middleSection = new Icon(15, 30);
+    private final Icon rightBorder   = new Icon(15, 30);
+    private final Icon upArrow       = new Icon(20, 20);
+    private final Icon downArrow     = new Icon(20, 20);
+    
+    private Color topColor;
+    private Color bottomColor;
     
     public SpinBox(float xOffset, float yOffset, float width, float parentPosX, float parentPosY) {
         super(xOffset, yOffset, width, parentPosX, parentPosY);
@@ -29,8 +39,17 @@ public class SpinBox extends TextInput {
         leftBorder.setSubImage(2, 2);
         leftBorder.setColor(Color.WHITE);
 
+        middleSection.setSubImage(4, 2);
+        middleSection.setColor(Color.WHITE);
+        
         rightBorder.setSubImage(3, 2);
         rightBorder.setColor(Color.WHITE);
+        
+        upArrow.setSubImage(6, 0);
+        upArrow.setColor(Color.RGME_WHITE);
+        
+        downArrow.setSubImage(7, 0);
+        downArrow.setColor(Color.RGME_WHITE);
     }
 
     @Override
@@ -162,6 +181,9 @@ public class SpinBox extends TextInput {
             }
         }
         
+        topColor    = getButtonColor(mouse, topButton);
+        bottomColor = getButtonColor(mouse, bottomButton);
+        
         return null;
     }
 
@@ -169,6 +191,8 @@ public class SpinBox extends TextInput {
     public void render(Program uiProgram, Background background, FreeTypeFont font) {
         background.drawRectangle(bounds, Color.RGME_LIGHT_GRAY, uiProgram);
         background.drawRectangle(front, Color.RGME_DARK_GRAY, uiProgram);
+        background.drawRectangle(topButton, topColor, uiProgram);
+        background.drawRectangle(bottomButton, bottomColor, uiProgram);
         
         glEnable(GL_SCISSOR_TEST);
         glScissor((int) scissorBox.xPos, (int) scissorBox.yPos, (int) scissorBox.width, (int) scissorBox.height);
@@ -178,7 +202,11 @@ public class SpinBox extends TextInput {
         glDisable(GL_SCISSOR_TEST);
         
         leftBorder.render(uiProgram);
+        middleSection.render(uiProgram);
         rightBorder.render(uiProgram);
+        
+        upArrow.render(uiProgram);
+        downArrow.render(uiProgram);
     }
 
     @Override
@@ -198,8 +226,25 @@ public class SpinBox extends TextInput {
         scissorBox.width  = bounds.width;
         scissorBox.height = HEIGHT;
         
+        topButton.xPos    = bounds.xPos + bounds.width;
+        topButton.yPos    = bounds.yPos;
+        bottomButton.xPos = topButton.xPos;
+        bottomButton.yPos = bounds.yPos + bottomButton.height;
+        
+        upArrow.position.set(topButton.xPos + 2, topButton.yPos + 17);
+        downArrow.position.set(bottomButton.xPos + 2, bottomButton.yPos + 18);
+        
         leftBorder.position.set(bounds.xPos, bounds.yPos + HEIGHT);
-        rightBorder.position.set(bounds.xPos + (bounds.width - 14), leftBorder.position.y);
+        middleSection.position.set(bounds.xPos + (bounds.width - PADDING), leftBorder.position.y);
+        rightBorder.position.set(bounds.xPos + bounds.width + 9, leftBorder.position.y);
     }
 
+    private Color getButtonColor(Mouse mouse, Rectangle button) {
+        if(button.contains(mouse.cursorPos)) {
+            return (mouse.clicked) ? Color.RGME_SILVER : Color.RGME_LIGHT_GRAY;
+        } else {
+            return Color.RGME_MEDIUM_GRAY;
+        }
+    }
+    
 }
