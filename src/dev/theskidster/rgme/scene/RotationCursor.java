86b3ebcf20e8c5f49ee33ab91e877a2b0315beff
theskidster <theskidster@gmail.com>
2021-03-30 19:53:12 -0500

    added circle selection

--- src/dev/theskidster/rgme/scene/RotationCursor.java
+++ src/dev/theskidster/rgme/scene/RotationCursor.java
@@ -16,17 +16,23 @@ final class RotationCursor {
     private int circleID;
     
     private final float RADIUS = 0.5f;
+    private float epsilon;
     
     private final Vector3f position = new Vector3f();
-    private final Circle[] circles  = new Circle[1];
+    private final Vector3f[] planes = new Vector3f[3];
+    private final Circle[] circles  = new Circle[3];
     
     RotationCursor() {
         circles[0] = new Circle(RADIUS, 3, Color.RGME_RED);
-        //circles[1] = new Circle(RADIUS, 3, Color.RGME_GREEN);
-        //circles[2] = new Circle(RADIUS, 3, Color.RGME_BLUE);
+        circles[1] = new Circle(RADIUS, 3, Color.RGME_GREEN);
+        circles[2] = new Circle(RADIUS, 3, Color.RGME_BLUE);
         
         circles[0].rotation.y = 90f;
-        //circles[2].rotation.x = 90f;
+        circles[2].rotation.x = 90f;
+        
+        planes[0] = new Vector3f(1, 0, 0);
+        planes[1] = new Vector3f(0, 0, 1);
+        planes[2] = new Vector3f(0, 1, 0);
     }
     
     public void update(Vector3f objectPos) {
@@ -43,13 +49,49 @@ final class RotationCursor {
     }
     
     void selectCircle(Vector3f camPos, Vector3f camRay) {
-        boolean result;
+        epsilon = (float) Math.sqrt(
+                    Math.pow((position.x - camPos.x), 2) + 
+                    Math.pow((position.y - camPos.y), 2) +
+                    Math.pow((position.z - camPos.z), 2)) *
+                    0.004f;
         
+        for(int c = 0; c < circles.length; c++) {
+            /*
+            To determine which circle has been selected I first check if the ray 
+            provided by the camera intersects the plane associated with the circle.
+            */
+            float distanceToPlane = Intersectionf.intersectRayPlane(camPos, camRay, position, planes[c], 1f);
+            
+            /*
+            If an intersection between the ray and plane has been detected, we'll
+            perform another test using the distance between the point of intersection
+            and position of the circle to see if the ray falls close to its radius.
+            */
+            if(distanceToPlane > 0) {
+                Vector3f intersectionPoint = new Vector3f();
+                camPos.add(camRay.mul(distanceToPlane), intersectionPoint);
+
+                float distanceToOrigin = intersectionPoint.distance(position);
+                
+                if(distanceToOrigin <= (RADIUS + epsilon) && distanceToOrigin >= (RADIUS - epsilon)) {
+                    circleID = c;
+                    System.out.println(circleID);
+                    break;
+                }
+            }
+        }
+    }
+    
+    Movement moveCircle(Vector3f camDir, Vector3f rayChange) {
+        Movement movement = new Movement();
         
+        switch(circleID) {
+            case 0 -> {
+                
+            }
+        }
         
-        //result = Intersectionf.testRaySphere(camPos, camRay, position, RADIUS);
-        
-        //System.out.println(result);
+        return movement;
     }
     
 }
